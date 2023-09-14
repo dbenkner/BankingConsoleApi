0fc31ce0402981c7c71383cf4caff454d1ce827f
Josh Stothfang <josh@joshstothfang.com>
2023-09-14 10:24:34 -0400

    Worked on MakeDeposit method

---
+++ BankingConsoleApi/Controllers/TransactionsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BankingConsoleApi.Controllers
+{
+    public class TransactionsController : GeneralController
+    {
+        public async Task MakeDeposit(HttpClient _http, JsonSerializerOptions joptions, IEnumerable<Account> accounts) 
+        {
+            var amount = ReadAndWrite("Amount to deposit: ");
+            var accountId = ReadAndWrite("Account to deposit to: ");
+        }
+    }
+}
